9cf6404ec0f2208d71611e94bf4fc582555ad0dd
HoaDuongg <hoadvph32358@fpt.edu.vn>
2023-12-04 15:52:29 +0700

    fix Comments

--- src/Views/client/ProductDetail/index.php
+++ src/Views/client/ProductDetail/index.php
@@ -1,4 +1,24 @@
+<?php
+include 'CommentFunctions.php';
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $id_user = $_SESSION['account']['id_user']; 
+    $id_pro = $_POST['idpro']; 
+    $content = $_POST['commentContent']; 
 
+    $isInserted = insertComment($id_user, $id_pro, $content); 
+
+    if ($isInserted) {
+        echo $content; 
+        exit; 
+    } else {
+        echo "Có lỗi xảy ra khi gửi bình luận.";
+        exit; 
+    }
+}
+
+$productId = $_GET['id']; 
+$comments = getComments($productId);
+?>
 <style>
     * {
         /* box-shadow: 0 0 5px #000; */
@@ -124,6 +144,7 @@
                         <div class="preview-pic tab-content">
                             <div class="tab-pane active" id="pic-1"><img src="assets/files/assets/images/<?= $productCurrent['image_urls'][0] ?>" alt="Ảnh sản phẩm"></div>
                         </div>
+
                         <div class="listImage d-flex col-12">
                             <?php foreach ($productCurrent['image_urls'] as $key => $image) : ?>
                                 <img class="col-3 mx-0 p-2 <?= $key == 0 ? 'active' : '' ?>" src="assets/files/assets/images/<?= $image ?>" />
@@ -173,7 +194,12 @@
             </div>
         </div>
     </div>
-    <div class="commentUsers container">
+    <div class="product-description container my-4">
+        <h3 class=" fw-bolder mt-3">Mô tả sản phẩm </h3>
+        <?= empty($productCurrent['description']) ? 'Chưa có mô tả' :  $productCurrent['description'] ?>
+    </div>
+ <!-- Nhận xét  -->
+ <div class="commentUsers container">
         <h3 class="fw-bolder mt-3">Nhận xét sản phẩm từ khách hàng</h3>
         <br>
         <div class="show_comment">
@@ -210,3 +236,85 @@
         </div>
     </div>
 </main>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $('#commentForm').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "", 
+            data: $(this).serialize(),
+            success: function(response){
+                alert('Bình luận đã được gửi!');
+                location.reload();
+            },
+            error: function(xhr, status, error){
+                console.error(error);
+            }
+        });
+    });
+});
+</script>
+<script>
+    const containerImage = document.querySelector(".listImage");
+    const btnMinus = document.querySelector("#minus");
+    const btnPlus = document.querySelector("#plus");
+    const quantity = document.querySelector("#quantity");
+    const color = document.querySelector("#colors");
+    const size = document.querySelector("#sizes");
+    const btnAddCart = document.querySelector(".add-to-cart");
+
+    var index = 0;
+    (() => {
+        const images = document.querySelectorAll(".listImage img");
+        const imageActive = document.querySelector(".preview-pic img");
+        images.forEach((image, indexImage) => {
+            image.addEventListener("click", () => {
+                images.forEach((img) => img.classList.remove("active"));
+                image.classList.add("active");
+                imageActive.src = image.src;
+                index = indexImage;
+
+            })
+        })
+    })();
+
+    btnMinus.addEventListener("click", () => {
+        if (quantity.value > 1) {
+            quantity.value--;
+        }
+    })
+    btnPlus.addEventListener("click", () => {
+        quantity.value++;
+    })
+    quantity.addEventListener("change", () => {
+        if (quantity.value < 1) {
+            quantity.value = 1;
+        }
+    })
+    btnAddCart.addEventListener("click", () => {
+        const productId = btnAddCart.dataset.id;
+        const userId = btnAddCart.getAttribute("data-idUser");
+        if (size.value == "" || color.value == "") {
+            alert("Vui lòng chọn size và màu sắc");
+            return;
+        }
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
+
+        xhr.open("POST", "/api/carts", true);
+        formData.append("idProduct", productId);
+        formData.append("idUser", userId);
+        formData.append("size", size.value);
+        formData.append("color", color.value);
+        formData.append("quantity", quantity.value);
+
+        xhr.send(formData);
+        xhr.onload = () => {
+            alert("Thêm vào giỏ hàng thành công");
+            const data =JSON.parse(xhr.responseText);
+            document.querySelector(".cart_count").innerHTML = data;
+        };
+    })
+</script>
