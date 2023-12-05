295f6fc20ef1aa69215eb4ea00b1d7be25d0298d
ZoroSs168 <quangdat15102003@gmail.com>
2023-12-05 17:25:46 +0700

    biến thể

--- src/Views/admin/carts/index.php
+++ src/Views/admin/carts/index.php
@@ -16,7 +16,7 @@
                         <li class="breadcrumb-item">
                             <a href="/addmin/dashboard"><i class="feather icon-home"></i></a>
                         </li>
-                        <li class="breadcrumb-item"><a href="#!">Giỏ hàng</a> </li>
+                        <li class="breadcrumb-item"><a href="#!">Đơn hàng</a> </li>
                     </ul>
                 </div>
             </div>
@@ -32,9 +32,7 @@
                         <div class="col-sm-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h5>Danh sách Giỏ hàng</h5>
-
-                                    <a href="/admin/carts/create" class="btn btn-info btn-sm">Tạo mới</a>
+                                    <h5>Danh sách đơn hàng</h5>
                                 </div>
                                 <div class="card-block">
                                     <div class="dt-responsive table-responsive">
@@ -44,6 +42,7 @@
                                                     <th>STT</th>
                                                     <th>Sản phẩm</th>
                                                     <th>Số lượng</th>
+                                                    <th>Trạng thái</th>
                                                     <th>Khách hàng</th>
                                                     <th>Chức năng</th>
                                                 </tr>
@@ -53,25 +52,34 @@
                                                 <?php foreach ($showData as $key => $cart) : ?>
                                                     <tr>
                                                         <td><?= $key + 1 ?></td>
-                                                        <td>
+                                                        <td> 
                                                         <span>Tên sản phẩm: </span><?= $cart['name_product'] ?><br>
                                                         <span>Tên danh mục: </span><?= $cart['name_category'] ?><br>
-                                                        <span>Giá: </span><?= $cart['price'] ?><br>
-                                                        <span>Ảnh sản phẩm: </span><img src="<?= $cart['img'] ?>" alt="Ảnh sản phẩm">
                                                         </td>
                                                         <td><?= $cart['quantity'] ?></td>
+                                                        <td><?php
+                                                            if($cart['status'] == 0){
+                                                                echo "Đang chờ xử lý";
+                                                            } else if($cart['status'] == 1){
+                                                                echo "Đã thanh toán";
+                                                            } else if($cart['status'] == 2){
+                                                                echo "Đã giao hàng";
+                                                            } else if($cart['status'] == 3){
+                                                                echo "Đã hủy";
+                                                            }
+                                                        ?></td>
                                                         <td>
                                                             <span>Tên tài khoản: </span><?= $cart['user_name'] ?><br>
                                                             <span>Tên khách hàng: </span><?= $cart['name'] ?><br>
-                                                            <span>Ảnh khách hàng: </span><img src="<?= $cart['img'] ?>" alt="Ảnh khách hàng"><br>
+                                                            <span>Ảnh khách hàng: </span><img src="/assets/files/assets/images/<?= $cart['image'] ?>" alt="Ảnh khách hàng" width="100"><br>
                                                             <span>Email: </span><?= $cart['email'] ?><br>
                                                             <span>Địa chỉ:</span><?= $cart['address'] ?><br>
                                                             <span>Số điện thoại: </span><?= $cart['phone'] ?>
                                                         </td>
                                                         <td>
-                                                            <a href="/admin/carts/update?id=<?= $cart['cart_id'] ?>" class="btn btn-primary btn-sm">Cập nhật</a>
+                                                            <a href="/admin/carts/update?id=<?= $cart['id'] ?>" class="btn btn-primary btn-sm">Cập nhật</a>
 
-                                                            <form action="/admin/carts/delete?id=<?= $cart['cart_id'] ?>" method="post">
+                                                            <form action="/admin/carts/delete?id=<?= $cart['id'] ?>" method="post">
                                                                 <button type="submit" onclick="return confirm('Bạn có chắc chắn xóa?');" class="btn btn-danger btn-sm mt-2">Xóa</button>
                                                             </form>
                                                         </td>
