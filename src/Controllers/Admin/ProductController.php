ff8cb5a1860b32cadcb5b2d091519e25e8d08800
ZoroSs168 <quangdat15102003@gmail.com>
2023-12-05 18:03:55 +0700

    sửa models

--- src/Controllers/Admin/ProductController.php
+++ src/Controllers/Admin/ProductController.php
@@ -3,7 +3,9 @@
 namespace MVC_DA1\Controllers\Admin;
 
 use MVC_DA1\Controller;
+use MVC_DA1\Models\Categories_Properties;
 use MVC_DA1\Models\Category;
+use MVC_DA1\Models\Images;
 use MVC_DA1\Models\Product;
 
 class ProductController extends Controller
@@ -17,7 +19,7 @@ class ProductController extends Controller
     public function __construct()
     {
         $this->productModel = (new Product);
-        $this->allProducts = $this->productModel->allProductsTypes([['products.id', 'DESC']]);
+        $this->allProducts = $this->productModel->allProductsTypes([['p.id', 'DESC']]);
         $this->allCategories = (new Category)->all();
     }
     public function index()
@@ -26,29 +28,24 @@ class ProductController extends Controller
             if (!empty($products['image_urls'])) {
                 $products['image_urls'] = explode(",", $products['image_urls']);
             }
-
-            if (!empty($products['sizes'])) {
-                $products['sizes'] = explode(",", $products['sizes']);
-            }
-
-            if (!empty($products['colors'])) {
-                $products['colors'] = explode(",", $products['colors']);
-            }
         }
+        $allProductsProperties = (new Categories_Properties)->all();
 
         $this->renderAdmin(
             'products/index',
             [
                 'products' => $this->allProducts,
+                'allProductsProperties' => $allProductsProperties
             ]
         );
     }
     public function create()
     {
+
+
         if (isset($_POST['btn-submit'])) {
             $data = [
                 'name_product' => $_POST['nameProduct'],
-                'price' => $_POST['price'],
                 'description' => $_POST['description'],
                 'view' => 0,
                 'id_category' => $_POST['categories'],
@@ -58,51 +55,88 @@ class ProductController extends Controller
 
             $this->productModel->insert($data);
 
+
             $idProduct = $this->productModel->getLastId();
 
-            $images = $_FILES['image_urls']['name'];
+            $properties = [
+                'size' => $_POST['size'],
+                'color' => $_POST['color'],
+                'price' => $_POST['price'],
+                'quantity' => $_POST['quantity'],
+                'id_product' => $idProduct
+            ];
 
-            foreach ($images as $image) {
-                $this->productModel->insertImages($idProduct, $image);
+            (new Categories_Properties)->insert($properties);
+
+
+            $files = $_FILES['image_urls'];
+            $files_name = $files['name'];
+
+            foreach ($files_name as $key => $file) {
+                move_uploaded_file($files['tmp_name'][$key], "assets/files/assets/images/" . $file);
+                (new Images)->uploadImages($idProduct, $file);
             }
-            
+
+            // $images = $_FILES['image_urls']['name'];
+
+            // foreach ($images as $image) {
+            //     $this->productModel->insertImages($idProduct, $image);
+            // }
+
+            header('Location: /admin/products');
         }
 
         $this->renderAdmin(
             'products/create',
             [
                 'allCategories' => $this->allCategories,
-
             ]
         );
     }
 
-    public function update() {
+    public function update()
+    {
         $id = $_GET['id'];
 
         $productCurrent = $this->productModel->getProductCurrent($id);
+        $propertiesCurrent = (new Categories_Properties)->getPropertiesByProductId($id);
 
-        if($productCurrent['image_urls']) {
+        if ($productCurrent['image_urls']) {
             $productCurrent['image_urls'] = explode(",", $productCurrent['image_urls']);
         }
-        if($productCurrent['colors']) {
+        if ($productCurrent['colors']) {
             $productCurrent['colors'] = explode(",", $productCurrent['colors']);
         }
-        if($productCurrent['sizes']) {
+        if ($productCurrent['sizes']) {
             $productCurrent['sizes'] = explode(",", $productCurrent['sizes']);
+        }
+
+        if(isset($_POST['btn-submit'])) {
+            $properties = [
+                'size' => $_POST['size'],
+                'color' => $_POST['color'],
+                'price' => $_POST['price'],
+                'quantity' => $_POST['quantity'],
+                'id_product' => $id
+            ];
+            echo "<pre>";
+            print_r($properties);
+            die;
         }
 
         $this->renderAdmin('products/update', [
             'allCategories' => $this->allCategories,
-            'productCurrent' => $productCurrent
+            'productCurrent' => $productCurrent,
+            'propertiesCurrent' => $propertiesCurrent
         ]);
     }
 
-    public function delete() {
+    public function delete()
+    {
         $conditions = [
             ['id', '=', $_GET['id']],
         ];
-        
+
         (new Product())->delete($conditions);
 
         header('Location: /admin/products');
